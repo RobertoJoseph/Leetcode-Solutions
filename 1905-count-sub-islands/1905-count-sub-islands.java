8c1984256115cc863dfb408b20b624a47d5955b0
RobertoJoseph <67745591+RobertoJoseph@users.noreply.github.com>
2022-06-04 16:17:58 +0200

    Time: 38 ms (35.12%), Space: 143.5 MB (10.11%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.java
+++ 1905-count-sub-islands/1905-count-sub-islands.java
@@ -1,24 +1,43 @@
 class Solution {
-  public int countSubIslands(int[][] grid1, int[][] grid2) {
-    int ans = 0;
+    int m = 0, n = 0;
+    
+    public int countSubIslands(int[][] grid1, int[][] grid2) {
+        m = grid1.length;
+        n = grid1[0].length;
+        int subIslands = 0;
 
-    for (int i = 0; i < grid2.length; ++i)
-      for (int j = 0; j < grid2[0].length; ++j)
-        if (grid2[i][j] == 1)
-          ans += dfs(grid1, grid2, i, j);
+      
+        
+        // fill grid1 and grid2
+        for(int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                if (grid2[i][j] == 1)
+                    if (dfs(grid1, grid2, i, j))
+                        subIslands++;
+            }
+        }
+        
+        return subIslands;
+    }
+    
+    int[] dirs = new int[]{0,1,0,-1,0};
+    
+    public boolean dfs(int[][] grid1, int[][] grid2, int x, int y){
+        if (x < 0 || x >= m || y < 0 || y >= n || grid2[x][y] == 0)
+            return true;
+        
+        // return false if grid2 has an island, but grid1 doesn't
+        if (grid1[x][y] == 0)
+            return false;
+        
+      
+        grid2[x][y] = 0;
+        boolean isSubisland = true;
 
-    return ans;
-  }
-
-  private int dfs(int[][] grid1, int[][] grid2, int i, int j) {
-    if (i < 0 || i == grid1.length || j < 0 || j == grid2[0].length)
-      return 1;
-    if (grid2[i][j] != 1)
-      return 1;
-
-    grid2[i][j] = 2; // mark 2 as visited
-
-    return dfs(grid1, grid2, i + 1, j) & dfs(grid1, grid2, i - 1, j) &
-           dfs(grid1, grid2, i, j + 1) & dfs(grid1, grid2, i, j - 1) & grid1[i][j];
-  }
+        for(int i = 0; i < 4; i++){
+            isSubisland = dfs(grid1, grid2, x + dirs[i], y + dirs[i + 1]) &isSubisland;
+        }
+        
+        return isSubisland;
+    }
 }
