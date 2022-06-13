f2d78398b7b3e05ea95ed3545d41c828321e73c5
RobertoJoseph <67745591+RobertoJoseph@users.noreply.github.com>
2022-06-13 02:44:15 +0200

    Time: 11 ms (48.23%), Space: 54.2 MB (68.80%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.java
@@ -1,73 +1,69 @@
 class Solution {
-   
-    private static List<List<Integer>> ans;
-
-    public static boolean findValueInPair(Pair x, HashSet<Pair> hs) {
-        for (Pair temp : hs) {
-            if (temp.x == x.x && temp.y == x.y) return true;
-        }
-        return false;
+     int[][] count;
+    List<List<Integer>> list; 
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+     list = new ArrayList<>();
+      int r = heights.length;
+        int c = heights[0].length;
+       count = new int[r][c];
+        pacific(heights, count);
+        atlantic(heights, count);
+       for(int i = 0;i<r;i++){
+         for(int j = 0;j<c;j++){
+           if(count[i][j]==2){
+              List<Integer> arr = new ArrayList<>();
+             arr.add(i);
+             arr.add(j);
+             list.add(new ArrayList<>(arr));
+           }
+         }
+       }
+        return list;
     }
+  
+    public void dfs(int[][] grid, boolean[][] visited, int r, int c, int p,  int[][] count){
+        if(r>=grid.length||r<0|c>=grid[0].length||c<0)return;
+        if(visited[r][c]==true || grid[r][c]<p)return;
+        
+        visited[r][c] = true;
+        count[r][c] += 1;
+        p = grid[r][c];
+        dfs(grid, visited, r-1, c, p, count);
+       dfs(grid, visited, r+1, c, p, count);
+       dfs(grid, visited, r, c-1, p, count);
+       dfs(grid, visited, r, c+1, p, count);
 
-    public static List<List<Integer>> pacificAtlantic(int[][] heights) {
-        ans = new ArrayList<>();
-        HashSet<Pair> pac = new HashSet<>();
-        HashSet<Pair> atl = new HashSet<>();
-        int rows = heights.length;
-        int cols = heights[0].length;
-        for (int c = 0; c < heights[0].length; c++) {
-            dfs(0, c, heights, heights[0][c], pac);
-            dfs(rows - 1, c, heights, heights[rows - 1][c], atl);
-        }
-        for (int r = 0; r < heights.length; r++) {
-            dfs(r, 0, heights, heights[r][0], pac);
-            dfs(r, cols - 1, heights, heights[r][cols - 1], atl);
-        }
-        for (int i = 0; i < heights.length; i++) {
-            for (int j = 0; j < heights[0].length; j++) {
-                Pair x = new Pair(i, j);
-                if (findValueInPair(x, atl) && findValueInPair(x, pac))
-                    ans.add(new ArrayList<>(Arrays.asList(i, j)));
+        
+    }
+    public void pacific(int[][] heights, int[][] count){
+        int r = heights.length;
+        int c = heights[0].length;
+       boolean[][] visited = new boolean[r][c];
+       for(int i=0;i<c;i++){
+            if(visited[0][i]==false){
+                dfs(heights,visited,0,i,-1, count);
             }
         }
-        return ans;
-    }
-
-    public static void dfs(int i, int j, int[][] heights, int prevHeight, HashSet<Pair> visited) {
-        if (i < 0 || j < 0 || i >= heights.length
-            || j >= heights[0].length || findValueInPair(new Pair(i, j), visited) ||
-            heights[i][j] < prevHeight) return;
-        visited.add(new Pair(i, j));
-        dfs(i + 1, j, heights, heights[i][j], visited);
-        dfs(i - 1, j, heights, heights[i][j], visited);
-        dfs(i, j + 1, heights, heights[i][j], visited);
-        dfs(i, j - 1, heights, heights[i][j], visited);
-    }
-
-    public static void main(String[] args) {
-        int[][] heights = {{1, 2, 2, 3, 5}, {3, 2, 3, 4, 4}, {2, 4, 5, 3, 1}, {6, 7, 1, 4, 5}, {5, 1, 1, 2, 4}};
-        System.out.println(pacificAtlantic(heights));
-    }
-
-    public static class Pair implements Comparable<Pair> {
-        int x, y;
-
-        public Pair(int x, int y) {
-            this.x = x;
-            this.y = y;
-
+        for(int i=0;i<r;i++){
+            if(visited[i][0]==false ){
+                dfs(heights,visited,i,0,-1, count);
+            }
         }
-
-
-        public int compareTo(Pair A) {
-            return this.y > A.y ? -1 : 1; // sorting ascending
-        }
-
-        @Override
-        public String toString() {
-            return "{" + x + "," + y + '}';
-        }
-
     }
-
+  
+   public void atlantic(int[][] heights, int[][] count){
+        int r = heights.length;
+        int c = heights[0].length;
+       boolean[][] visited = new boolean[r][c];
+        for(int i=0;i<r;i++){
+            if(visited[i][c-1]==false){
+                dfs(heights,visited,i,c-1, -1, count);
+            }
+        }
+        for(int i=0;i<c;i++){
+            if(visited[r-1][i]==false){
+                dfs(heights,visited,r-1,i, -1, count);
+            }
+        }
+    }
 }
