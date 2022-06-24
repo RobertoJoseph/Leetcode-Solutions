e4aedae9972e5fe71e9cdf22a5e18fad0468f231
RobertoJoseph <67745591+RobertoJoseph@users.noreply.github.com>
2022-06-24 15:58:26 +0200

    Time: 132 ms (12.29%), Space: 55.5 MB (18.27%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.java
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.java
@@ -1,49 +1,20 @@
 class Solution {
    
    
-    public static int removeduplicates(int a[], int n) {
-        if (n == 0 || n == 1) {
-            return n;
-        }
-
-        // creating another array for only storing
-        // the unique elements
-        int[] temp = new int[n];
-        int j = 0;
-
-        for (int i = 0; i < n - 1; i++) {
-            if (a[i] != a[i + 1]) {
-                temp[j++] = a[i];
-            }
-        }
-
-        temp[j++] = a[n - 1];
-
-        // Changing the original array
-        for (int i = 0; i < j; i++) {
-            a[i] = temp[i];
-        }
-
-        return j;
-    }
-
     public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
-
-        TreeSet<Integer> ts = new TreeSet<>();
+        HashSet<Integer> ts = new HashSet<>();
         for (int x : nums2) {
             ts.add(x);
         }
         ArrayList<Integer> list1 = new ArrayList<>();
         ArrayList<Integer> list2 = new ArrayList<>();
-
-
         for (int i = 0; i < nums1.length; i++) {
             if (!ts.contains(nums1[i])) {
                 if (!list1.contains(nums1[i]))
                     list1.add(nums1[i]);
             }
         }
-        TreeSet<Integer> ts2 = new TreeSet<>();
+        HashSet<Integer> ts2 = new HashSet<>();
         for (int x : nums1) {
             ts2.add(x);
         }
