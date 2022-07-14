0ce493c03227b1671c0c7a3865e7ec6a2b5cc7ea
RobertoJoseph <67745591+RobertoJoseph@users.noreply.github.com>
2022-07-14 13:07:07 +0200

    Time: 45 ms (40.50%), Space: 53.5 MB (51.30%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -1,24 +1,27 @@
 class Solution {
-    public String minRemoveToMakeValid(String s) {
+    
+    public static String minRemoveToMakeValid(String s) {
         Stack<Integer> st = new Stack<>();
-        
-        char ch[] = s.toCharArray();
-        
-        for(int i = 0; i < ch.length; i++){
-            if(ch[i] == '(') st.push(i);
-            else if(ch[i] == ')'){
-                if(st.isEmpty()) ch[i] = '#';
-                else st.pop();
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') st.push(i);
+            else if (s.charAt(i) == ')') {
+                if (st.isEmpty())
+                    chars[i] = '#';
+                else
+                    st.pop();
             }
         }
-        while(!st.isEmpty()){
-            ch[st.pop()] = '#';
+        while (!st.isEmpty()) {
+            chars[st.pop()] = '#';
         }
         StringBuilder sb = new StringBuilder();
-        
-        for(int i = 0; i < ch.length; i++){
-            if(ch[i] != '#') sb.append(ch[i]);
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == '#') continue;
+            ;
+            sb.append(chars[i]);
         }
         return sb.toString();
     }
+
 }
