6b75646e55fa1f4aaf0a6e34b9eb1851785863d8
RobertoJoseph <67745591+RobertoJoseph@users.noreply.github.com>
2022-07-15 13:28:52 +0200

    Time: 31 ms (21.81%), Space: 69.7 MB (10.25%) - LeetHub

--- 415-add-strings/415-add-strings.java
+++ 415-add-strings/415-add-strings.java
@@ -1,78 +1,29 @@
 class Solution {
-    
-    public static String addStrings(String num1, String num2) {
+    public String addStrings(String num1, String num2) {
+        
+        int i = num1.length()-1;
+        int j = num2.length()-1;
         int carry = 0;
-        String result = "";
-
-        if (num1.length() < num2.length()) {
-            int j = num2.length() - 1;
-            for (int i = num1.length() - 1; i >= 0; i--, j--) {
-                int x1 = num1.charAt(i) - '0';
-                int x2 = num2.charAt(j) - '0';
-                int sum = x1 + x2 + carry;
-                //It is two integers
-                if (sum >= 10 && sum <= 99) {
-                    result = sum % 10 + result;
-                    carry = Integer.parseInt(Integer.toString(sum).substring(0, 1));
-                } else {
-                    result = sum + result;
-                    carry = 0;
-                }
-
+        String result= "";
+        
+        while(i >= 0 || j >= 0 || carry == 1) {
+            int x = 0;
+            int y = 0;
+            if(i >= 0)
+                x = num1.charAt(i) - '0';
+            if(j >= 0)
+                y = num2.charAt(j) - '0';
+            int sum = x + y + carry;
+            carry = 0;
+            if(sum > 9) {
+                carry = 1;
+                result = sum%10 + result;
+            } else {
+                result = sum + result;
             }
-            int startIndex = num2.length() - num1.length() - 1;
-            while (startIndex >= 0) {
-                int x = num2.charAt(startIndex) - '0';
-                x = x + carry;
-                if (x >= 10 && x <= 99) {
-                    result = x % 10 + result;
-                    carry = Integer.parseInt(Integer.toString(x).substring(0, 1));
-                } else {
-                    result = x + result;
-                    carry = 0;
-                }
-
-                startIndex--;
-            }
-            if (carry != 0) result = carry + result;
-
-
-        } else {
-            int j = num1.length() - 1;
-            for (int i = num2.length() - 1; i >= 0; i--, j--) {
-                int x1 = num1.charAt(j) - '0';
-                int x2 = num2.charAt(i) - '0';
-                int sum = x1 + x2 + carry;
-                //It is two integers
-                if (sum >= 10 && sum <= 99) {
-                    result = sum % 10 + result;
-                    carry = Integer.parseInt(Integer.toString(sum).substring(0, 1));
-                } else {
-                    result = sum + result;
-                    carry = 0;
-                }
-
-            }
-            int startIndex = num1.length() - num2.length() - 1;
-            while (startIndex >= 0) {
-                int x = num1.charAt(startIndex) - '0';
-                x = x + carry;
-                if (x >= 10 && x <= 99) {
-                    result = x % 10 + result;
-                    carry = Integer.parseInt(Integer.toString(x).substring(0, 1));
-                } else {
-                    result = x + result;
-                    carry = 0;
-                }
-
-                startIndex--;
-            }
-            if (carry != 0) result = carry + result;
-
-
+            i--;
+            j--;
         }
         return result;
-
     }
-
 }
