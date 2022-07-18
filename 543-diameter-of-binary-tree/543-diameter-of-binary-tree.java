96c2fa9ab2486bddd542ff71523e71e444d21fef
RobertoJoseph <67745591+RobertoJoseph@users.noreply.github.com>
2022-07-18 23:14:53 +0200

    Time: 0 ms (100.00%), Space: 42.1 MB (87.49%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.java
@@ -1,22 +1,19 @@
 class Solution {
     private int diameter;
     public int diameterOfBinaryTree(TreeNode root) {
-        diameter = 0;
+       diameter = 0;
         longestPath(root);
         return diameter;
     }
     private int longestPath(TreeNode node){
-        if(node == null) return 0;
-        // recursively find the longest path in
-        // both left child and right child
-        int leftPath = longestPath(node.left);
-        int rightPath = longestPath(node.right);
-
-        // update the diameter if left_path plus right_path is larger
-        diameter = Math.max(diameter, leftPath + rightPath);
-
-        // return the longest one between left_path and right_path;
-        // remember to add 1 for the path connecting the node and its parent
-        return Math.max(leftPath, rightPath) + 1;
-    }
+        if(node==null)return 0;
+        
+        int leftHeight = longestPath(node.left);
+        int rightHeight = longestPath(node.right);
+        
+        diameter = Math.max(diameter,leftHeight+rightHeight);
+        
+        return 1+Math.max(leftHeight,rightHeight);
+  
+}
 }
