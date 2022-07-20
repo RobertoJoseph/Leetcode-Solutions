74f66107cc6f806d7a6f3a227149dd76d90be2f8
RobertoJoseph <67745591+RobertoJoseph@users.noreply.github.com>
2022-07-20 15:09:23 +0200

    Time: 3 ms (27.08%), Space: 42.3 MB (61.08%) - LeetHub

--- 408-valid-word-abbreviation/408-valid-word-abbreviation.java
+++ 408-valid-word-abbreviation/408-valid-word-abbreviation.java
@@ -1,32 +1,23 @@
 class Solution {
-    public static boolean validWordAbbreviation(String word, String abbr) {
-        int pt1 = 0;
-        int pt2 = 0;
-        String res = "";
-        int x = 0;
-        while (pt1 < word.length() && pt2 < abbr.length()) {
-            String con = "";
-            if (word.charAt(pt1) == abbr.charAt(pt2)) {
-                res += word.charAt(pt1);
-                pt1++;
-                pt2++;
-            } else if (word.charAt(pt1) != abbr.charAt(pt2)
-                && !Character.isDigit(abbr.charAt(pt2))) {
-                return false;
-            } else if (Character.isDigit(abbr.charAt(pt2)) && abbr.charAt(pt2) != '0') {
-                while (pt2 < abbr.length() && Character.isDigit(abbr.charAt(pt2))) {
-                    con += abbr.charAt(pt2);
-                    pt2++;
+   // O(n + m) time | O(1) space
+    public boolean validWordAbbreviation(String word, String abbr) {
+        int i = 0;
+        int j = 0;
+        while(i < word.length() && j < abbr.length()) {
+            if(Character.isLetter(abbr.charAt(j))) {
+                if(word.charAt(i) != abbr.charAt(j)) return false;
+                i++;
+                j++;
+            } else {
+                int number = 0;
+                while(j < abbr.length() && Character.isDigit(abbr.charAt(j))) {
+                    number *=10;
+                    number += abbr.charAt(j++) - '0';
+                    if(number == 0) return false;
                 }
-                x = Integer.parseInt(con);
-                while (pt1 < word.length() && x-- > 0) {
-                    res += word.charAt(pt1);
-                    pt1++;
-                }
-                if (x > 0) return false;
-            }else return false;
+                i += number;
+            }
         }
-        if (res.length() == word.length()&&pt1==word.length()&&pt2==abbr.length()) return true;
-        return false;
+        return i == word.length() && j == abbr.length();
     }
 }
