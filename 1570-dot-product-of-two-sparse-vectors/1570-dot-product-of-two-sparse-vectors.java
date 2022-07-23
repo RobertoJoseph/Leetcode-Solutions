220c0d68da73c2d71bdb12efdc44dc9a5431b09c
RobertoJoseph <67745591+RobertoJoseph@users.noreply.github.com>
2022-07-23 14:01:52 +0200

    Time: 16 ms (38.12%), Space: 119.6 MB (46.16%) - LeetHub

--- 1570-dot-product-of-two-sparse-vectors/1570-dot-product-of-two-sparse-vectors.java
+++ 1570-dot-product-of-two-sparse-vectors/1570-dot-product-of-two-sparse-vectors.java
@@ -1,25 +1,26 @@
 class SparseVector {
-    
-    private int nums[];
+    // Map the index to value for all non-zero values in the vector
+    Map<Integer, Integer> mapping;      
+
     SparseVector(int[] nums) {
-        this.nums = nums;
-        
+        mapping = new HashMap<>();
+        for (int i = 0; i < nums.length; ++i) {
+            if (nums[i] != 0) {
+                mapping.put(i, nums[i]);        
+            }
+        }
     }
-    
-	// Return the dotProduct of two sparse vectors
-    public int dotProduct(SparseVector vec) {
-        int result =0;
-        for(int i =0; i<nums.length; i++){
-            int mul =nums[i]*vec.nums[i];
-            result+=mul;
+
+    public int dotProduct(SparseVector vec) {        
+        int result = 0;
+
+        // iterate through each non-zero element in this sparse vector
+        // update the dot product if the corresponding index has a non-zero value in the other vector
+        for (Integer i : this.mapping.keySet()) {
+            if (vec.mapping.containsKey(i)) {
+                result += this.mapping.get(i) * vec.mapping.get(i);
+            }
         }
         return result;
-        
-        
     }
 }
-
-// Your SparseVector object will be instantiated and called as such:
-// SparseVector v1 = new SparseVector(nums1);
-// SparseVector v2 = new SparseVector(nums2);
-// int ans = v1.dotProduct(v2);
