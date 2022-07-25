befd20a31a6affe4dcba59d1bc71a0227b3ba157
RobertoJoseph <67745591+RobertoJoseph@users.noreply.github.com>
2022-07-25 17:17:50 +0200

    Time: 24 ms (29.17%), Space: 56 MB (5.03%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -1,29 +1,29 @@
 class Solution {
     public int[] topKFrequent(int[] arr, int k) {
         
-        HashMap<Integer,Integer>map = new HashMap<>();
+       Map<Integer,Integer>map = new HashMap<>();
         for(int i =0; i<arr.length; i++){
             map.put(arr[i],map.getOrDefault(arr[i],0)+1);
         }
+        
         List<Integer>[]buckets = new List[arr.length+1];
         for(int i =0; i<buckets.length; i++){
             buckets[i]= new ArrayList<>();
         }
-        for(int key: map.keySet()){
+        for(int key:map.keySet()){
             buckets[map.get(key)].add(key);
         }
         List<Integer>flattened = new ArrayList<>();
         for(int i = buckets.length-1; i>=0; i--){
-            for(int num : buckets[i]){
+            for(int num :buckets[i]){
                 flattened.add(num);
             }
         }
-        int[]result = new int[k];
+        int []result = new int[k];
         for(int i =0; i<k; i++){
-            result[i]=flattened.get(i);
+            result[i]= flattened.get(i);
         }
         return result;
-        
      
     }
 }
