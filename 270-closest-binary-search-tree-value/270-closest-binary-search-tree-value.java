578a1fef343ac93daf1154df0cf894497ca26897
RobertoJoseph <67745591+RobertoJoseph@users.noreply.github.com>
2022-07-25 18:33:04 +0200

    Time: 0 ms (100.00%), Space: 43.4 MB (70.97%) - LeetHub

--- 270-closest-binary-search-tree-value/270-closest-binary-search-tree-value.java
+++ 270-closest-binary-search-tree-value/270-closest-binary-search-tree-value.java
@@ -1,42 +1,11 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    double min = Double.MAX_VALUE;
-    int node;
-    public int closestValue(TreeNode root, double target) {
-        bfs(root,target);
-        return node;
+  public int closestValue(TreeNode root, double target) {
+    int val, closest = root.val;
+    while (root != null) {
+      val = root.val;
+      closest = Math.abs(val - target) < Math.abs(closest - target) ? val : closest;
+      root =  target < root.val ? root.left : root.right;
     }
-    
-    public void bfs(TreeNode root,double target){
-         Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        while(!queue.isEmpty()){
-            TreeNode cur = queue.poll();
-            if(Math.abs(cur.val-target)<min){
-                min = Math.abs(cur.val-target);
-                node = cur.val;
-            }
-            if(cur.left!=null){
-                queue.add(cur.left);
-            }
-            if(cur.right!=null)queue.add(cur.right);
-        }
-    }
-    
-    
-    
+    return closest;
+  }
 }
