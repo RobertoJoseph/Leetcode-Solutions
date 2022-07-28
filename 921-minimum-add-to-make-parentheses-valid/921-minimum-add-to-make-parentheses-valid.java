b5dae9227f3739e79ec0f39957a7fe7e68d7e6a9
RobertoJoseph <67745591+RobertoJoseph@users.noreply.github.com>
2022-07-28 16:59:32 +0200

    Time: 1 ms (89.16%), Space: 42.1 MB (47.52%) - LeetHub

--- 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.java
+++ 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.java
@@ -1,24 +1,15 @@
 class Solution {
-    public int minAddToMakeValid(String s) {
-        Stack<Character>st = new Stack<>();
-        int open =0;
-        int close =0;
-        char[]ch = s.toCharArray();
-        for(int i =0; i<ch.length; i++){
-            if(ch[i]=='('){
-                open++;
-                st.push(ch[i]);}
-            else{
-                if(!st.isEmpty()){
-                    st.pop();
-                    open--;
-                }else{
-                    close++;
-                }
+    public int minAddToMakeValid(String S) {
+        int ans = 0, bal = 0;
+        for (int i = 0; i < S.length(); ++i) {
+            bal += S.charAt(i) == '(' ? 1 : -1;
+            // It is guaranteed bal >= -1
+            if (bal == -1) {
+                ans++;
+                bal++;
             }
-        
+        }
+
+        return ans + bal;
     }
-        return open+close;
-        
-}
 }
