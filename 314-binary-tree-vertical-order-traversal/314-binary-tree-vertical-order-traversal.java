46e766db00e26292750baef6d3a4ec50823fa5e4
RobertoJoseph <67745591+RobertoJoseph@users.noreply.github.com>
2022-07-28 15:45:26 +0200

    Time: 5 ms (52.72%), Space: 43.1 MB (77.58%) - LeetHub

--- 314-binary-tree-vertical-order-traversal/314-binary-tree-vertical-order-traversal.java
+++ 314-binary-tree-vertical-order-traversal/314-binary-tree-vertical-order-traversal.java
@@ -1,40 +1,40 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
 class Solution {
   public List<List<Integer>> verticalOrder(TreeNode root) {
-        
-     List<List<Integer>>list = new ArrayList<>();
-      if(root==null)return list;
-      Map<Integer,ArrayList>columnTable = new HashMap();
-      Queue<Pair<TreeNode,Integer>>queue = new LinkedList<>();
-      queue.add(new Pair(root,0));
-      int column;
-      while(!queue.isEmpty()){
-         Pair<TreeNode, Integer> p = queue.poll();
-          root = p.getKey();
-          column = p.getValue();
-          if(!columnTable.containsKey(column)){
-              columnTable.put(column,new ArrayList<Integer>());
-          }
-          columnTable.get(column).add(root.val);
-          if(root.left!=null)queue.add(new Pair(root.left,column-1));
-          if(root.right!=null)queue.add(new Pair(root.right,column+1));
-         
-          
+    List<List<Integer>> output = new ArrayList();
+    if (root == null) {
+      return output;
+    }
+
+    Map<Integer, ArrayList> columnTable = new HashMap();
+    // Pair of node and its column offset
+    Queue<Pair<TreeNode, Integer>> queue = new ArrayDeque();
+    int column = 0;
+    queue.offer(new Pair(root, column));
+
+    int minColumn = 0, maxColumn = 0;
+
+    while (!queue.isEmpty()) {
+      Pair<TreeNode, Integer> p = queue.poll();
+      root = p.getKey();
+      column = p.getValue();
+
+      if (root != null) {
+        if (!columnTable.containsKey(column)) {
+          columnTable.put(column, new ArrayList<Integer>());
+        }
+        columnTable.get(column).add(root.val);
+        minColumn = Math.min(minColumn, column);
+        maxColumn = Math.max(maxColumn, column);
+
+        queue.offer(new Pair(root.left, column - 1));
+        queue.offer(new Pair(root.right, column + 1));
       }
-      List<Integer>sortedKeys = new ArrayList<Integer>(columnTable.keySet());
-      Collections.sort(sortedKeys);
-      for(int k : sortedKeys){
-          list.add(columnTable.get(k));
-      }
-      return list;
- 
+    }
+
+    for(int i = minColumn; i < maxColumn + 1; ++i) {
+      output.add(columnTable.get(i));
+    }
+
+    return output;
   }
 }
