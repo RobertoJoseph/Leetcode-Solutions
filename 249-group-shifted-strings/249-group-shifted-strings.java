0c543573734b08fc3449fe749cf0d861e8759b4b
RobertoJoseph <67745591+RobertoJoseph@users.noreply.github.com>
2022-08-13 13:44:04 +0200

    Time: 3 ms (78.25%), Space: 43.9 MB (54.27%) - LeetHub

--- 249-group-shifted-strings/249-group-shifted-strings.java
+++ 249-group-shifted-strings/249-group-shifted-strings.java
@@ -1,36 +1,29 @@
 class Solution {
-    // Create a hash value
-    String getHash(String s) {
-        char[] chars = s.toCharArray();
-        StringBuilder hashKey = new StringBuilder();
-        
-        for (int i = 1; i < chars.length; i++) {
-            hashKey.append((char) ((chars[i] - chars[i - 1] + 26)%26  ));
-        }
-        
-        return hashKey.toString();
-    }
     
+    public  String getPattern(String word) {
+        StringBuilder stb = new StringBuilder();
+        char[] chars = word.toCharArray();
+         for (int i = 1; i < chars.length; i++) {
+            stb.append((char) ((chars[i] - chars[i - 1] + 26)%26  ));
+        }
+        return stb.toString();
+    }
+  
     public List<List<String>> groupStrings(String[] strings) {
-        Map<String, List<String>> mapHashToList = new HashMap<>();
-        
-        // Create a hash_value (hashKey) for each string and append the string
-        // to the list of hash values i.e. mapHashToList["cd"] = ["acf", "gil", "xzc"]
-        for (String str : strings ) {
-            String hashKey = getHash(str);
-            if (mapHashToList.get(hashKey) == null) {
-                mapHashToList.put(hashKey, new ArrayList<>());
-            } 
-            mapHashToList.get(hashKey).add(str);
+     
+        List<List<String>>result = new LinkedList<>();
+        Map<String,List<String>>map = new HashMap<>();
+    
+        for(String word : strings){
+            String pattern = getPattern(word);
+            if(!map.containsKey(pattern))
+                map.put(pattern,new LinkedList<>());
+            map.get(pattern).add(word);
         }
-        
-        // Iterate over the map, and add the values to groups
-        List<List<String>> groups = new ArrayList<>();
-        for (List<String> group : mapHashToList.values()) {
-            groups.add(group);
+        for(List<String>group:map.values()){
+            result.add(group);
         }
-        
-        // Return a list of all of the grouped strings
-        return groups;
+        return result;
     }
 }
+  
