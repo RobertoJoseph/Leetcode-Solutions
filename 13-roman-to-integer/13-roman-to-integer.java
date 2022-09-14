50ccfd93578ed3c1f7ca1a38cb43ee277f5037b6
RobertoJoseph <67745591+RobertoJoseph@users.noreply.github.com>
2022-09-14 12:08:37 +0200

    Time: 14 ms (25.24%), Space: 47.4 MB (18.57%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.java
+++ 13-roman-to-integer/13-roman-to-integer.java
@@ -1,36 +1,42 @@
 class Solution {
+    
+    static Map<String, Integer> values = new HashMap<>();
+
+    static {
+        values.put("I", 1);
+        values.put("V", 5);
+        values.put("X", 10);
+        values.put("L", 50);
+        values.put("C", 100);
+        values.put("D", 500);
+        values.put("M", 1000);
+        values.put("IV", 4);
+        values.put("IX", 9);
+        values.put("XL", 40);
+        values.put("XC", 90);
+        values.put("CD", 400);
+        values.put("CM", 900);
+    }
 
     public int romanToInt(String s) {
-        Map<Character, Integer> hs = new HashMap<Character, Integer>() {
-
-            {
-                put('I', 1);
-                put('V', 5);
-                put('X', 10);
-                put('L', 50);
-                put('C', 100);
-                put('D', 500);
-                put('M', 1000);
+        
+        int sum = 0;
+        int i = 0;
+        while (i < s.length()) {
+            if (i < s.length() - 1) {
+                String doubleSymbol = s.substring(i, i + 2);
+                // Check if this is the length-2 symbol case.
+                if (values.containsKey(doubleSymbol)) {
+                    sum += values.get(doubleSymbol);
+                    i += 2;
+                    continue;
+                }
             }
-        };
-        int result = 0;
-        char[] chrs = s.toCharArray();
-        int i =0;
-        while (i < chrs.length - 1) {
-            if (hs.get(chrs[i + 1]) > hs.get(chrs[i])) {
-                result += (hs.get(chrs[i + 1]) - hs.get(chrs[i]));
-                i+=2;
-            } else {
-                result += (hs.get(chrs[i]));
-                i++;
-            }
+            // Otherwise, it must be the length-1 symbol case.
+            String singleSymbol = s.substring(i, i + 1);
+            sum += values.get(singleSymbol);
+            i += 1;
         }
-        int len = chrs.length;
-        if (len == 1) result += hs.get(chrs[0]);
-        if (len >= 2) {
-            System.out.println(hs.get(chrs[len - 1]));
-            if (hs.get(chrs[len - 1]) <= hs.get(chrs[len - 2])) result = result + hs.get(chrs[len - 1]);
-        }
-        return result;
+        return sum;
     }
 }
