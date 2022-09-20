5ee65a64dcd054095977c9312c18b97410037dfb
RobertoJoseph <67745591+RobertoJoseph@users.noreply.github.com>
2022-09-20 21:37:46 +0200

    Time: 14 ms (82.24%), Space: 44.3 MB (84.86%) - LeetHub

--- 482-license-key-formatting/482-license-key-formatting.java
+++ 482-license-key-formatting/482-license-key-formatting.java
@@ -1,19 +1,21 @@
 class Solution {
     public String licenseKeyFormatting(String s, int k) {
-        if(s == null || s.equals("")){
-            return "";
-        }
-        s = s.replaceAll("-", "").toUpperCase();
-        int count = 0;
-        StringBuilder sb = new StringBuilder();
-        for(int i = s.length() -1; i >=0; i--){
-            sb.append(s.charAt(i));
-            count++;
-            if(i != 0 && count == k){
-                sb.append("-");
-                count = 0;
+        StringBuilder sb = new StringBuilder(); 
+        
+        int count = k; 
+        for(int i=s.length()-1; i>=0; i--) {
+            if(s.charAt(i) !='-') {
+                if(count>0) {
+                    sb.append(Character.toUpperCase(s.charAt(i))); 
+                    count--;
+                }else {
+                    sb.append('-');
+                    sb.append(Character.toUpperCase(s.charAt(i))); 
+                    count = k-1;
+                }   
             }
         }
-        return sb.reverse().toString();
+        
+        return sb.reverse().toString(); 
     }
 }
