d568fbafb3b797082ccc9c1494382f60b5a361e9
RobertoJoseph <67745591+RobertoJoseph@users.noreply.github.com>
2022-09-24 00:14:21 +0200

    Time: 10 ms (23.48%), Space: 48.2 MB (49.14%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -1,12 +1,20 @@
-class Solution {
+public class Solution {
     public int[] countBits(int n) {
-      int[]dp = new int[n+1];
-        int offset = 1;
-        for(int i =1; i<=n; i++){
-            if(offset*2==i)offset=i;
-            dp[i]=1+dp[i-offset];
+        int[] ans = new int[n + 1];
+        int x = 0;
+        int b = 1;
+    
+        // [0, b) is calculated
+        while (b <= n) {
+            // generate [b, 2b) or [b, n) from [0, b)
+            while (x < b && x + b <= n) {
+                ans[x + b] = ans[x] + 1;
+                ++x;
+            }                         
+            x = 0; // reset x
+            b <<= 1; // b = 2b
         }
-        return dp;
-        
+                  
+        return ans;
     }
 }
