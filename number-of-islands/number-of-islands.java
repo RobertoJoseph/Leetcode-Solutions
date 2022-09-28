2e8c295a0526eb10b5ad9ef59963d40251b3e1aa
RobertoJoseph <67745591+RobertoJoseph@users.noreply.github.com>
2022-09-28 22:21:57 +0200

    Time: 10 ms, Memory: 58.2 MB - LeetHub

--- number-of-islands/number-of-islands.java
+++ number-of-islands/number-of-islands.java
@@ -1,21 +1,22 @@
 class Solution {
+    boolean visited[][];
   public int numIslands(char[][] grid) {
      
 
       final int N = grid.length, M = grid[0].length;
-      boolean visited[][] = new boolean[N][M];
+      visited = new boolean[N][M];
       
       int islandNum = 0;
       for (int i = 0; i < N; i++)
           for (int j = 0; j < M; j++)
               if (grid[i][j] == '1' && !visited[i][j]) {
                   islandNum++;
-                  dfs(i, j, grid, visited);
+                  dfs(i, j, grid);
               }
 
       return islandNum;
   }
-  private void dfs(int i, int j, char[][] grid, boolean[][] visited) {
+  private void dfs(int i, int j, char[][] grid) {
       if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length)
           return;
       if (grid[i][j] != '1' || visited[i][j])
@@ -23,9 +24,9 @@ class Solution {
 
       visited[i][j] = true;
       
-      dfs(i - 1, j, grid, visited);
-      dfs(i + 1, j, grid, visited);
-      dfs(i, j - 1, grid, visited);
-      dfs(i, j + 1, grid, visited);
+      dfs(i - 1, j, grid);
+      dfs(i + 1, j, grid);
+      dfs(i, j - 1, grid);
+      dfs(i, j + 1, grid);
   }
 }
